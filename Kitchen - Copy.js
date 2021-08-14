f92b4bdfd50c2a64f8fe6f90ff2b77170593bd3c
Kaetharan <78970710+Kaetharan@users.noreply.github.com>
2021-08-13 21:28:34 -0400

    Add files via upload

---
+++ Kitchen - Copy.js
@@ -0,0 +1,28 @@
+img="";
+status="";
+
+function setup(){
+canvas=createCanvas(640,420);
+canvas.center();
+objectDetector=ml5.objectDetector('cocossd',modelLoaded);
+document.getElementById("status").innerHTML="Status: Detecting Objects";
+}
+
+function modelLoaded(){
+    console.log("model loaded");
+    status=true;
+    objectDetector.detect(img, gotResult);
+}
+
+function preload(){
+    img=loadImage('Kitchen.jpg');
+}
+
+function gotResult(error, results){
+if(error){
+    console.error(error);
+}else{
+    console.log(results);
+
+}
+}
---
+++ home.js
@@ -0,0 +1,19 @@
+function livingRoom(){
+window.location="livingRoom.html";
+}
+
+function Desk(){
+window.location="Desk.html";
+}
+
+function Bedroom(){
+window.location="Bedroom.html";
+}
+
+function Kitchen(){
+window.location="Kitchen.html";
+}
+
+function Bathroom(){
+window.location="Bathroom.html";
+}
